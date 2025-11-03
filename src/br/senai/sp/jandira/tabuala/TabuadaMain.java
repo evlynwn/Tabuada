b4f15214662094b3d484687efd236d3097e38c1f
EVELLYN SANTIAGO DE SOUZA <25203674@CFP127ALUNO.LOCAL>
2025-11-03 16:58:43 -0300

    finalizado

--- src/br/senai/sp/jandira/tabuala/TabuadaMain.java
+++ src/br/senai/sp/jandira/tabuala/TabuadaMain.java
@@ -1,4 +1,13 @@
 package br.senai.sp.jandira.tabuala;
 
 public class TabuadaMain {
+    public static void main(String[] args) {
+
+        Tabuada tabuada = new Tabuada();
+        tabuada.Coletardados();
+
+    }
+
+
+
 }
